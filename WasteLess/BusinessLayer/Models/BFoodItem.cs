def3300bcb7af6026fdf638a671871f9447e6558
Tibi <tiberiu.andrei858@gmail.com>
2020-03-28 04:03:19 +0200

    first commit

---
+++ WasteLess/BusinessLayer/Models/BFoodItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Models
+{
+    public class BFoodItem
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int Calories { get; set; }
+
+        public DateTime PurchaseDate { get; set; }
+
+        public DateTime ExpDate { get; set; }
+
+        public DateTime? ConsDate { get; set; }
+
+        public long User_id { get; set; }
+
+    }
+}
