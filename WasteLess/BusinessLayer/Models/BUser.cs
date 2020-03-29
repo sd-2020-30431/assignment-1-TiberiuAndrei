898755e188e58501ce52881c623c2d056b760680
Tibi <tiberiu.andrei858@gmail.com>
2020-03-29 07:33:23 +0300

    Third Commit

---
+++ WasteLess/BusinessLayer/Models/BUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Models
+{
+    public class BUser
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Mail { get; set; }
+
+    }
+
+}
---
+++ WasteLess/DataAccessLayer/DataContext/DatabaseConnectionManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer.DataContext
+{
+    public class DatabaseConnectionManager : DbContext
+    {
+        /*public DatabaseConnectionManager(DbContextOptions<DatabaseConnectionManager> options) : base(options)
+        {
+
+        }*/
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-H7CJLP2;Initial Catalog=WasteLess;Integrated Security=True");
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<FoodItem> FoodItems { get; set; }
+
+        public DbSet<MailBot> MailBots { get; set; }
+
+    }
+}
